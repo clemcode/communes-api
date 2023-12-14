2095efe96aec87a0654b940b617c13de31e27ade
Georgina Berrezel <georgina.berrezel@gmail.com>
2023-12-14 15:45:07 +0100

    Create input and API call

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-apiUrl = "https://geo.api.gouv.fr/communes?nom="
+const apiUrl = "https://geo.api.gouv.fr/communes?nom="
+
+async function getTown(name) {
+    const response = await fetch(apiUrl + name);
+    const data = await response.json();
+    console.log(data);
+}
+
+const townInput = document.getElementById('town');
+const sendButton = document.getElementById('send');
+
+sendButton.addEventListener('click', async () => {
+    await getTown(townInput.value)
+});
