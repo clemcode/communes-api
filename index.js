d97fe99e2d71483091efa2b152f3761607d9b33b
Clément Tutin <clement.tutin.pro@gmail.com>
2023-12-14 16:31:41 +0100

    Ajout click sur la liste -> affichage des infos de l'élément clické

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 const apiUrl = "https://geo.api.gouv.fr/communes?nom="
 
+const townInput = document.getElementById('town');
+const sendButton = document.getElementById('send');
+const townInfo = document.getElementById('townInformation');
+const townList = document.getElementById('townList');
+
 async function getTown(name) {
     const response = await fetch(apiUrl + name);
     const data = await response.json();
-    console.log(data);
     return data
 }
 
-const townInput = document.getElementById('town');
-const sendButton = document.getElementById('send');
-
-const townInfo = document.getElementById('information-town');
-
 sendButton.addEventListener('click', async () => {
-    
     const townData = await getTown(townInput.value)
-
     const nom = document.createElement('p');
     const codeDepartement = document.createElement('p');
     const population = document.createElement('p');
 
-    nom.textContent = townData[0].nom
-    codeDepartement.textContent = townData[0].codeDepartement
-    population.textContent = townData[0].population
-    townInfo.appendChild(nom)
-    townInfo.appendChild(codeDepartement)
-    townInfo.appendChild(population)
-});
+    townData.forEach((town, index) => {
+        const liNom = document.createElement('li');
+        const aNom = document.createElement('a');
+        aNom.textContent = town.nom
+        aNom.href = '#' + town.nom
+        aNom.addEventListener('click', (event)=> showInfo(event.target.textContent))
+        liNom.append(aNom)
+        townList.append(liNom)
+    });
 
+    function showInfo(town) {
+        const nomTown = townData.find((element) => element.nom === town)
+        nom.textContent = nomTown.nom
+        codeDepartement.textContent = nomTown.codeDepartement
+        population.textContent = nomTown.population
+        townInfo.appendChild(nom)
+        townInfo.appendChild(codeDepartement)
+        townInfo.appendChild(population)
+    }
+});
